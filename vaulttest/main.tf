708564133abc8f1e2bd2a346beacc4201a834546
Perumalla Bogeswar Gopinadh <perumallabogeswargopinadh@gmail.com>
2024-09-17 22:08:24 +0530

    creating infra

--- vaulttest/main.tf
+++ vaulttest/main.tf
@@ -10,10 +10,17 @@ data "vault_generic_secret" "example" {
 
 output "my_secret_value" {
   value = data.vault_generic_secret.example.data["SSH_PWD"]
-  sensitive = false 
 }
 
 variable "token" {
     default = "hvs.UdS9AFcv7b9XhLvGRVUFcexP"
 }
 
+ resource "null_resource" "provisi" {
+
+  connection { # this connection is used to remotly connect using SSH protocal and with user name and password.
+    type     = "ssh"
+    user     = "ec2-user"
+    password = data.vault_generic_secret.example.data["SSH_PWD"]
+    host     = aws_instance.instance1.public_ip # we dont need data.resources if its the resources that is going to be created with the current infra code.
+  }
